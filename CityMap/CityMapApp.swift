36c2082dcbf61bb1644da89ebabebe95f0fa0def
Javier Manzo <javier.r.manzo@gmail.com>
2024-12-11 09:29:33 -0300

    Init project

---
+++ CityMap/CityMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityMapApp.swift
+//  CityMap
+//
+//  Created by Javier Manzo on 11/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct CityMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CityMap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CityMap
+//
+//  Created by Javier Manzo on 11/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
